c749b63d1c4d3f3b49da2cbacaff6e34967f6519
vitoladev <asaptorvi@protonmail.com>
2021-04-30 18:23:20 -0300

    fix: wrong database path

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        entities: [__dirname + '../../**/*.entity{.ts,.js}'],
         synchronize: true,
       }),
     }),
