bba66391243c341e9ec91dd2a1eaf2f9bd11c97f
vitoladev <asaptorvi@protonmail.com>
2021-04-30 22:10:48 -0300

    feat: transform response interceptor

---
+++ src/core/http/transform-response.interceptor.ts
@@ -0,0 +1,47 @@
+import {
+  NestInterceptor,
+  Injectable,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { AbstractHttpAdapter, HttpAdapterHost } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { NestResponse } from './nest-response';
+
+@Injectable()
+export class TransformResponseInterceptor implements NestInterceptor {
+  private httpAdapter: AbstractHttpAdapter;
+
+  constructor(adapterHost: HttpAdapterHost) {
+    this.httpAdapter = adapterHost.httpAdapter;
+  }
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((controllerResponse: NestResponse) => {
+        if (controllerResponse instanceof NestResponse) {
+          const contexto = context.switchToHttp();
+          const response = contexto.getResponse();
+          const { headers, status, body } = controllerResponse;
+
+          const headersName = Object.getOwnPropertyNames(headers);
+
+          headersName.forEach((headerName) => {
+            const headerValue = headers[headerName];
+            this.httpAdapter.setHeader(response, headerName, headerValue);
+          });
+
+          this.httpAdapter.status(response, status);
+
+          return body;
+        }
+
+        return controllerResponse;
+      }),
+    );
+  }
+}
